0c647f2f9052f2afb7f14e082a39237c0059d3ce
You Wu <98357328+Wuyouuu@users.noreply.github.com>
2022-08-04 15:45:05 -0400

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -3,11 +3,26 @@
 clear
 
 # Check for git, and purge old version of i2ctools
+sudo apt-get install git-core
+sudo apt-get purge wiringpi
 sudo apt-get update
 sudo apt-get purge i2c-tools
 sudo apt-get install i2c-tools
 hash -r
 
-# Display WiringPi information
+# Install at desktop for easy access
+git clone https://github.com/WiringPi/WiringPi.git
+cd WiringPi
+git pull origin
+./build
+
+# Display i2c information
+echo -e "Installed successfully."
 echo -e "\n==================================================================="
 sudo i2cdetect -y 1
+
+# Display WiringPi information
+echo -e "\n==================================================================="
+gpio -v
+echo -e "\n==================================================================="
+gpio readall
